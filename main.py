ef694a0d191e1c86d435d1d1ff0a8fb7b4bf8f44
Berke Sayan <berke.sayann@gmail.com>
2022-08-19 21:46:33 +0300

    Text to speech python

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import gtts
 from playsound import playsound
-
+lines = []
+with open('Döküman.txt') as f:
+    lines += f.readlines()
 # make request to google to get synthesis
-tts = gtts.gTTS("Hello world")
+tts = gtts.gTTS(lines[0])
 
 # save the audio file
 tts.save("hello.mp3")
